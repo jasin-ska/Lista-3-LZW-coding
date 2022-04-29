0ec44889787589f9189f823e621dfd594c002beb
jasin-ska <lenajas0110@gmail.com>
2022-04-29 03:36:00 +0200

    fixed

--- src/Codes/Code.java
+++
@@ -1,9 +0,0 @@
-package Codes;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public interface Code {
-    List<Boolean> outputCoded(int idx);
-    ArrayList<Integer> decodeInput(ArrayList<Boolean> input);
-}
