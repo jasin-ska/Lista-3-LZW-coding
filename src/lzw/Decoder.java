83d7f023513fe1fc5a66303873e5a381c114bded
jasin-ska <74596198+jasin-ska@users.noreply.github.com>
2022-04-29 03:25:40 +0200

    working codes

---
+++ src/lzw/Decoder.java
@@ -0,0 +1,67 @@
+package lzw;
+
+import lzw.Dictionaries.DecodingDictionary;
+import lzw.Dictionaries.Dictionary;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Decoder {
+    public static void decode(ArrayList<Integer> indexes) throws IOException {
+        Dictionary dictionary = new DecodingDictionary();
+        dictionary.initDictionary();
+        int oldIdx = indexes.get(0);
+        List<Byte> seq = dictionary.getSequenceAt(oldIdx);
+        //System.out.println("OldIdx: " + oldIdx);
+        /*byte next = dictionary.getSequenceAt(indexes.get(1)).get(0);
+        List<Byte> combined0 = new ArrayList<>(dictionary.getSequenceAt(oldIdx).size() + 1);
+        combined0.addAll(dictionary.getSequenceAt(oldIdx));
+        combined0.add(next);
+        dictionary.addSequence(combined0);*/
+        int idx;
+        //System.out.println("Seq: " + seq);
+        BitsIO.outputSeq(seq);
+        for (int i = 0; i < indexes.size() - 1; i++) {
+            idx = indexes.get(i + 1);
+            //System.out.println("decoding idx: " + idx);
+            //System.out.println("seq from dict: " + dictionary.getSequenceAt(idx));
+            if (dictionary.getSequenceAt(idx) == null) {
+                //seq = dictionary.getSequenceAt(oldIdx);
+                List<Byte> combined = new ArrayList<>(dictionary.getSequenceAt(oldIdx).size() + 1);
+                //System.out.println("combined size: " + (seq.size() + 1));
+                combined.addAll(dictionary.getSequenceAt(oldIdx));
+                combined.add(seq.get(0));
+                dictionary.addSequence(combined);
+                //System.out.println("Added seq: " +combined);
+                BitsIO.outputSeq(combined);
+            }
+            else {
+                seq = dictionary.getSequenceAt(idx);
+                //System.out.println("!!!Seq: " + seq);
+                BitsIO.outputSeq(seq);
+                //System.out.println("Outputed seq: " + seq);
+                List<Byte> combined = new ArrayList<>(dictionary.getSequenceAt(oldIdx).size() + 1);
+                //System.out.println("combined size: " + (seq.size() + 1));
+                combined.addAll(dictionary.getSequenceAt(oldIdx));
+                combined.add(seq.get(0));
+                dictionary.addSequence(combined);
+            }
+
+            //next = seq.get(0);
+            //System.out.println("SQ: " + seq);
+            //System.out.println("next: " + next);
+            //System.out.println("oldIdx: " + oldIdx);
+            //List<Byte> combined = new ArrayList<>(seq.size() + 1);
+            //System.out.println("combined size: " + (seq.size() + 1));
+            //combined.addAll(dictionary.getSequenceAt(oldIdx));
+            //combined.add(next);
+            //dictionary.addSequence(combined);
+            //System.out.println("Just added seq: " + combined);
+            oldIdx = idx;
+        }
+
+
+
+    }
+}
---
+++ src/lzw/Test.java
@@ -0,0 +1,118 @@
+package lzw;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
+
+public class Test {
+
+    private static final String testDirectory = "./testy";
+
+    private static boolean isDecodedEqual(String name, String ext, char code) throws IOException {
+        String filePath = testDirectory + "/" + name + "/" + name + "." + ext;
+        String decodedFilePath = testDirectory + "/" + name + "/" + name + "-decoded-" + code + "." + ext;
+
+        byte[] fileBefore = Files.readAllBytes(Paths.get(filePath));
+        byte[] fileDecoded = Files.readAllBytes(Paths.get(decodedFilePath));
+        return Arrays.equals(fileBefore, fileDecoded);
+    }
+
+    public static void main(String[] args) throws IOException {
+        if(args.length < 1) {
+            System.out.println("Usage: java Test [fileName] (coding)");
+            return;
+        }
+        System.out.println("File: " + args[0]);
+
+        String fileNameExt = args[0];
+        String fileName = args[0].substring(0, fileNameExt.indexOf('.'));
+        String ext = args[0].substring(fileNameExt.indexOf('.')+1);
+
+        if(args.length == 2) {
+            if(!Objects.equals(args[1], "all")) {
+                BitsIO.setCode(args[1]);
+                testFile(fileName, ext, args[1].charAt(0));
+            }
+            else {
+                allCodesTest(fileName, ext);
+            }
+        }
+        else testFile(fileName, ext, 'o');
+    }
+
+    static void allCodesTest(String fileName, String ext) throws IOException {
+        System.out.println("omega.....................");
+        testFile(fileName, ext, 'o');
+        System.out.println("delta.....................");
+        BitsIO.setCode("delta");
+        testFile(fileName, ext, 'd');
+        System.out.println("gamma.....................");
+        BitsIO.setCode("gamma");
+        testFile(fileName, ext, 'g');
+        System.out.println("fibonacci.................");
+        BitsIO.setCode("fib");
+        testFile(fileName, ext, 'f');
+    }
+
+    static void testFile(String name, String ext, char code) throws IOException {
+        BitsIO.reset();
+        String filePath = testDirectory + "/" + name + "/" + name + "." + ext;
+        String codedFilePath = testDirectory + "/" + name + "/" + name + "-coded-" + code + "." + ext;
+        String decodedFilePath = testDirectory + "/" + name + "/" + name + "-decoded-" + code + "." + ext;
+
+        //encode
+        System.out.print("Encoding: ");
+        BitsIO.reset();
+        BitsIO.initCodedOutputFile(codedFilePath);
+        byte[] input = BitsIO.getCodingInput(filePath);
+        long cStart = System.currentTimeMillis();
+        Encoder.encode(input);
+        BitsIO.finishByte();
+        long cStop = System.currentTimeMillis();
+        System.out.println("DONE");
+        long encodingTime = cStop - cStart;
+
+        BitsIO.reset();
+
+        //decode
+        System.out.print("Decoding: ");
+        BitsIO.reset();
+        BitsIO.initDecodedOutputFile(decodedFilePath);
+        ArrayList<Integer> indexes = BitsIO.getDecodingInput(codedFilePath);
+        long dStart = System.currentTimeMillis();
+        Decoder.decode(indexes);
+        long dStop = System.currentTimeMillis();
+        System.out.println("DONE");
+        long decodingTime = dStop - dStart;
+
+        File fileBefore = new File(filePath);
+        long sizeBefore = fileBefore.length();
+        File fileCoded = new File(codedFilePath);
+        long sizeCoded = fileCoded.length();
+        File fileDecoded = new File(decodedFilePath);
+        long sizeDecoded = fileDecoded.length();
+
+        float entropyBefore = Entropy.calculateEntropy(filePath);
+        float entropyCoded = Entropy.calculateEntropy(codedFilePath);
+        float CompressionRate = (float)sizeBefore/sizeCoded;
+
+
+        System.out.println("STATS ---------");
+        System.out.println("\tCompression rate: " + CompressionRate);
+        System.out.println("\tEntropy: " + entropyBefore + "  -->  " + entropyCoded);
+        System.out.println("\tSize: " + sizeBefore + "  -->  " + sizeCoded);
+        System.out.println("\t(size decoded: " + sizeDecoded + ")");
+        System.out.println("TIME ------------");
+        System.out.println("\tCompression: " + encodingTime/1000f + " s");
+        System.out.println("\tDecompression: " + decodingTime/1000f + " s");
+
+        System.out.println(isDecodedEqual(name, ext, code)? "ORIGINAL = DECODED" : "DECODING ERROR");
+    }
+
+
+}
